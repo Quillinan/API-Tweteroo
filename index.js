99fbab0cbc285d2e0972c2f17d5a16c1a363f6bb
unknown <milton.quillinan1798@gmail.com>
2023-06-23 04:51:40 -0300

    bonus

--- index.js
+++ index.js
@@ -21,16 +21,39 @@ function addUser(username, avatar) {
 app.post('/sign-up', (req, res) => {
   const { username, avatar } = req.body;
 
+  if (
+    !username ||
+    !avatar ||
+    typeof username !== 'string' ||
+    typeof avatar !== 'string'
+  ) {
+    return res.status(400).send('Bad Request');
+  }
+
   const response = addUser(username, avatar);
 
   res.status(201).send(response);
 });
 
 app.post('/tweets', (req, res) => {
-  const { username, tweet } = req.body;
+  let { username, tweet } = req.body;
 
-  if (!username || !users.includes(username)) {
+  if (!username) {
+    username = decodeURIComponent(req.headers.user);
+  }
+
+  if (!username) {
     return res.status(401).send('UNAUTHORIZED');
+  }
+
+  const userExists = users.some((user) => user.username === username);
+
+  if (!userExists) {
+    return res.status(401).send('UNAUTHORIZED');
+  }
+
+  if (!tweet || typeof tweet !== 'string') {
+    return res.status(400).send('Bad Request');
   }
 
   const newTweet = {
@@ -63,6 +86,26 @@ app.get('/tweets', (_, res) => {
   });
 
   res.json(formattedTweets);
+});
+
+app.get('/tweets/:username', (req, res) => {
+  const { username } = req.params;
+  const userTweets = tweets.filter((tweet) => tweet.username === username);
+
+  const formattedTweets = userTweets.map(({ username, tweet }) => {
+    const user = users.find((user) => user.username === username);
+    return {
+      username,
+      avatar: user ? user.avatar : null,
+      tweet,
+    };
+  });
+
+  res.status(200).json(formattedTweets);
+});
+
+app.get('/users', (_, res) => {
+  res.status(200).json(users);
 });
 
 app.listen(5000, () => {
